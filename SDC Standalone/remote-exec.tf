619278e86ca33e853f826182a8dcf943174bedff
Mike Carley <Mike@streamsets.com>
2018-10-04 17:49:07 -0400

    Update remote-exec.tf

--- SDC Standalone/remote-exec.tf
+++ SDC Standalone/remote-exec.tf
@@ -10,6 +10,8 @@ resource "oci_core_instance" "DataCollector" {
         private_key = "${var.ssh_private_key}"
       }
       inline = [
+	"yum install java-1.8.0-openjdk.x86_64 -y",
+	"ulimit -n 32768",
 	"wget https://s3-us-west-2.amazonaws.com/archives.streamsets.com/datacollector/3.5.0/rpm/el7/streamsets-datacollector-3.5.0-el7-all-rpms.tar--2018-10-04 15:45:46--  https://s3-us-west-2.amazonaws.com/archives.streamsets.com/datacollector/3.5.0/rpm/el7/streamsets-datacollector-3.5.0-el7-all-rpms.tar",
   	"tar xf streamsets-datacollector-3.5.0-el7-all-rpms.tar",
  	"yum localinstall streamsets-datacollector-3.5.0-1.noarch.rpm",
