5416d6256cf50f56fd2e34cedda6b6c01fac3b2b
Mike Carley <Mike@streamsets.com>
2018-10-04 17:30:22 -0400

    Update remote-exec.tf

--- SDC Standalone with EDH Cluster/remote-exec.tf
+++ SDC Standalone with EDH Cluster/remote-exec.tf
@@ -60,7 +60,7 @@ resource "null_resource" "cdh-setup" {
       inline = [
   "wget https://s3-us-west-2.amazonaws.com/archives.streamsets.com/datacollector/3.5.0/rpm/el7/streamsets-datacollector-3.5.0-el7-all-rpms.tar--2018-10-04 15:45:46--  https://s3-us-west-2.amazonaws.com/archives.streamsets.com/datacollector/3.5.0/rpm/el7/streamsets-datacollector-3.5.0-el7-all-rpms.tar",
   "tar xf streamsets-datacollector-3.5.0-el7-all-rpms.tar",
-  "yum localinstall streamsets*.rpm",
+  "yum localinstall streamsets-datacollector-3.5.0-1.noarch.rpm",
   "systemctl start sdc",
   "echo The default username and password are admin and admin",
   "echo Browse to http://<system-ip>:18630/"
